ce44130fd813b6bf26d36c6228118f94198a46b5
anshyyy <anshuman9998@gmail.com>
2023-01-02 13:52:58 +0530

    added skeleton project

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
+
+const setUpandStartServer=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
+
+
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT}`)
+    });
+}
+
+setUpandStartServer();
