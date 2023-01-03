e8f5649a71e0831660791d3618c07867988010b4
anshyyy <anshuman9998@gmail.com>
2023-01-04 01:27:26 +0530

    Added models

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,12 +1,16 @@
 const sender = require('../config/emailConfig');
 
-const sendBasicEmail = (mailfrom,mailTo,mailSubject,mailBody) => {
-    sender.sendMail({
-        from:mailfrom,
-        to:mailTo,
-        subject:mailSubject,
-        text:mailBody
-    });
+const sendBasicEmail = async(mailfrom, mailTo, mailSubject, mailBody) => {
+    try {
+       const response = await sender.sendMail({
+            from: mailfrom,
+            to: mailTo,
+            subject: mailSubject,
+            text: mailBody
+        });
+    } catch (error) {
+         console.log(error);
+    }
 }
 module.exports = {
     sendBasicEmail
