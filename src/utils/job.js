ded52d819472d3c165bf364910566e18f3bbc9eb
anshyyy <anshuman9998@gmail.com>
2023-01-07 12:37:29 +0530

    Added notifaction sender

--- src/utils/job.js
+++ src/utils/job.js
@@ -1,18 +1,28 @@
 const cron = require('node-cron');
+const sender = require('../config/emailConfig');
 const EmailService = require('../services/ticket-service');
 const emailService = new EmailService();
 const setupJobs = () => {
-    cron.schedule('*/1 * * * *', async()=>{
-             const response = await emailService.get({status:"PENDING"});
-             response.forEach(email => {
-                emailService.sendBasicEmail(
-                "anshyytests@gmail.com",
-                email.recepientEmail,
-                email.subject,
-                email.content
-                );
-             });
-             console.log(response);
+    cron.schedule('*/1 * * * *', async () => {
+        const response = await emailService.get({ status: "PENDING" });
+        response.forEach(email => {
+            if (email.status != 'SUCCESS') {
+                sender.sendMail({
+                    to: email.recepientEmail,
+                    subject: email.subject,
+                    text: email.content
+                }, async (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        throw (err);
+                    } else {
+                        console.log(data);
+                        await emailService.updateTicket(email.id, { status: "SUCCESS" });
+                    }
+                });
+            }
+        });
+        console.log(response);
     });
 }
 
