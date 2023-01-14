48d60c0d5971d0782e4db3f8aabf5b40e70dcec8
anshyyy <anshuman9998@gmail.com>
2023-01-14 17:38:18 +0530

    set up message queues

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,5 +4,8 @@ dotenv.config();
 module.exports = {
     PORT : process.env.PORT,
     emailId: process.env.USER,
-    PASS:process.env.PASS
+    PASS:process.env.PASS,
+    MESSAGE_BROKER_URL:process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME:process.env.EXCHANGE_NAME,
+    REMAINDER_BINDING_KEY:process.env.REMAINDER_BINDING_KEY
 }
